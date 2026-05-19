d2f3fc200afa97392fa7b22cda627ac8fb2e050d
Alberto Perea León <eralon.dev@gmail.com>
2026-05-19 23:27:56 +0200

    feat: print hashed objects

--- src/data.py
+++ src/data.py
@@ -14,3 +14,8 @@ def hash_object(data):
     with open(f"{GUT_DIR}/objects/{oid}", "wb") as f:
         f.write(data)
     return oid
+
+
+def get_object(oid):
+    with open(f"{GUT_DIR}/objects/{oid}", "rb") as f:
+        return f.read()
