d9ad0b2e99b94cbf3beae36afcff1936bd7a17e7
Alberto Perea León <eralon.dev@gmail.com>
2026-05-20 12:37:52 +0200

    feat: log commits from HEAD

--- src/base.py
+++ src/base.py
@@ -1,4 +1,7 @@
+import itertools
+import operator
 import os
+from collections import namedtuple
 from . import data
 
 
@@ -75,6 +78,11 @@ def read_tree(tree_oid):
 
 def commit(message):
     commit = f"tree {write_tree()}\n"
+
+    HEAD = data.get_HEAD()
+    if HEAD:
+        commit += f"parent {HEAD}\n"
+
     commit += "\n"
     commit += f"{message}\n"
 
@@ -82,6 +90,27 @@ def commit(message):
     data.set_HEAD(oid)
 
     return oid
+
+
+Commit = namedtuple("Commit", ["tree", "parent", "message"])
+
+
+def get_commit(oid):
+    parent = None
+
+    commit = data.get_object(oid, "commit").decode()
+    lines = iter(commit.splitlines())
+    for line in itertools.takewhile(operator.truth, lines):
+        key, value = line.split(" ", 1)
+        if key == "tree":
+            tree = value
+        elif key == "parent":
+            parent = value
+        else:
+            assert False, f"Unknown field {key}"
+
+    message = "\n".join(lines)
+    return Commit(tree, parent, message)
 
 
 def is_ignored(path):
--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import textwrap
 from . import data
 from . import base
 
@@ -46,6 +47,10 @@ def parse_arguments():
     commit_parser.set_defaults(func=commit)
     commit_parser.add_argument("message")
 
+    log_parser = commands.add_parser("log")
+    log_parser.set_defaults(func=log)
+    log_parser.add_argument("oid", nargs="?")
+
     return parser.parse_args()
 
 
@@ -73,3 +78,15 @@ def read_tree(args):
 
 def commit(args):
     print(base.commit(args.message))
+
+
+def log(args):
+    oid = args.oid or data.get_HEAD()
+    while oid:
+        commit = base.get_commit(oid)
+
+        print(f"commit {oid}\n")
+        print(textwrap.indent(commit.message, "    "))
+        print("")
+
+        oid = commit.parent
